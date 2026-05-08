54d26fe6e8614a3cdca77346c7dfc5e886fe9bae
masulzen <masulzen@ncsu.edu>
2026-05-08 07:34:49 -0400

    add third party link to settings

--- src/ui.py
+++ src/ui.py
@@ -66,6 +66,11 @@ button_update_apikey = tk.Button(root, text="Update API key", command=update_api
 button_update_apikey.pack()
 tk.Label(root, textvariable=msg).pack()
 
+# 3rd party notices link
+label_link_thirdparty = tk.Label(root, text="Third Party Licenses", cursor='hand2', fg='blue')
+label_link_thirdparty.pack()
+label_link_thirdparty.bind('<Button-1>', lambda e: webbrowser.open(util.resource_path('THIRD-PARTY-NOTICES.txt')))
+
 def start(conf: config.SsTeXConfig):
     global cfg
     cfg = conf
